09bf9d721f60e8e20908479311400ea050a4c6b9
NSWell <paris3@163.com>
2019-07-05 16:04:20 +0800

    Upload project

---
+++ Tutorials/Assets/Scripts/JoystickController.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class JoystickController : MonoBehaviour
+{
+    //设置摇杆状态
+    //Floating：浮动，点击任意位置出现摇杆
+    //Fixed：固定，固定在某个位置上
+    public enum JoystickStateEnum
+    {
+        Floating,
+        Fixed
+    }
+
+    //摇杆出现的位置范围
+    //LeftHalfScreen:左半屏幕
+    //RightHalfScreen:右半屏幕
+    //FullScreen:全屏幕
+    public enum JoystickSafeAreaEnum
+    {
+        LeftHalfScreen,
+        RightHalfScreen,
+        FullScreen
+    }
+
+
+    public JoystickStateEnum joystickState;
+    public JoystickSafeAreaEnum joystickSafeArea;
+    
+    //摇杆的组成部分 image
+    [SerializeField] private Image JoystickBackground;
+    [SerializeField] private Image JoystickHandler;
+
+    //摇杆的背景Transform
+    private RectTransform JoystickBackgroundTransform;
+
+    //拖拽脚本
+    [SerializeField] private JoystickHandler JoystickHandlerScript;
+
+    //触摸的手指id
+    private int TouchUniqueId = 0;
+
+    //1/2屏幕范围
+    private float HalfScreen = Screen.width / 2;
+
+
+    private void Start()
+    {
+        JoystickBackgroundTransform = JoystickBackground.rectTransform;
+        SetupJoystickImageState(!(joystickState == JoystickStateEnum.Floating));
+        JoystickHandlerScript.SetJoystickHandler(JoystickHandler.rectTransform, 100);
+    }
+
+    /// <summary>
+    /// 此处需要用
+    /// </summary>
+    private void FixedUpdate()
+    {
+        if (Input.touchCount <= 0) return;
+        foreach (Touch tmp_Touch in Input.touches)
+        {
+            switch (tmp_Touch.phase)
+            {
+                case TouchPhase.Began:
+                    switch (joystickSafeArea)
+                    {
+                        case JoystickSafeAreaEnum.LeftHalfScreen:
+                            if (tmp_Touch.position.x < HalfScreen)
+                            {
+                                TouchUniqueId = tmp_Touch.fingerId;
+                                MoveInTouch(tmp_Touch);
+                            }
+
+                            break;
+                        case JoystickSafeAreaEnum.RightHalfScreen:
+                            if (tmp_Touch.position.x > HalfScreen)
+                            {
+                                TouchUniqueId = tmp_Touch.fingerId;
+                                MoveInTouch(tmp_Touch);
+                            }
+
+                            break;
+                        case JoystickSafeAreaEnum.FullScreen:
+                            TouchUniqueId = tmp_Touch.fingerId;
+                            MoveInTouch(tmp_Touch);
+                            break;
+                    }
+
+                    break;
+
+                case TouchPhase.Ended:
+                    //Hide joystick
+                    if (tmp_Touch.fingerId == TouchUniqueId)
+                        SetupJoystickImageState(false);
+                    break;
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// 触摸触发
+    /// </summary>
+    /// <param name="_touch">触摸位置</param>
+    private void MoveInTouch(Touch _touch)
+    {
+        switch (joystickState)
+        {
+            case JoystickStateEnum.Floating:
+                //将摇杆移动到当前触摸位置
+                Vector2 tmp_JoystickStartPosition = JoystickBackgroundTransform.position;
+                tmp_JoystickStartPosition.x = _touch.position.x;
+                tmp_JoystickStartPosition.y = _touch.position.y;
+                JoystickBackgroundTransform.position = tmp_JoystickStartPosition;
+
+                //重置摇杆knob中心点位置为当前触摸位置
+                JoystickHandlerScript.SetHandlerPositin(tmp_JoystickStartPosition);
+
+                //显示joystick
+                SetupJoystickImageState(true);
+                break;
+            case JoystickStateEnum.Fixed:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// 设置当前摇杆的显示状态
+    /// </summary>
+    /// <param name="_state">True:显示，False:不显示</param>
+    private void SetupJoystickImageState(bool _state)
+    {
+        JoystickBackground.enabled = _state;
+        JoystickHandler.enabled = _state;
+    }
+}
---
+++ Tutorials/Assets/Scripts/JoystickHandler.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JoystickHandler : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    //摇杆控制点
+    private RectTransform JoystickHandlerTransform;
+
+    //摇杆移动最大半径
+    private float MaxRadius = 100;
+
+    //摇杆初始化位置
+    private Vector2 OriginalPosition;
+
+
+    /// <summary>
+    /// 设置摇杆移动对象，移动半径等
+    /// </summary>
+    /// <param name="_handler">摇杆移动knob</param>
+    /// <param name="_radius">移动半径</param>
+    public void SetJoystickHandler(RectTransform _handler, float _radius)
+    {
+        JoystickHandlerTransform = _handler;
+        MaxRadius = _radius;
+    }
+
+
+    /// <summary>
+    /// 设置knob的初始位置
+    /// </summary>
+    /// <param name="_position">当前joystick的位置</param>
+    public void SetHandlerPositin(Vector2 _position)
+    {
+        OriginalPosition = _position;
+    }
+
+
+    /// <summary>
+    /// 用户拖拽输入
+    /// </summary>
+    /// <param name="_eventData"></param>
+    public void OnDrag(PointerEventData _eventData)
+    {
+        //计算滑动的位置与摇杆初始位置的朝向
+        Vector2 tmp_Direction = _eventData.position - OriginalPosition;
+
+        //移动的距离
+        float tmp_Distance = Vector3.Magnitude(tmp_Direction);
+
+        //限制移动的最大半径
+        float tmp_Radius = Mathf.Clamp(tmp_Distance, 0, MaxRadius);
+
+        //初始位置+ 移动的方向* 最大移动半径
+        JoystickHandlerTransform.position = OriginalPosition + tmp_Direction.normalized * tmp_Radius;
+    }
+
+
+    /// <summary>
+    /// 用户输入抬起
+    /// </summary>
+    /// <param name="_eventData"></param>
+    public void OnPointerUp(PointerEventData _eventData)
+    {
+        JoystickHandlerTransform.position = OriginalPosition;
+    }
+
+    /// <summary>
+    /// 用户输入按下
+    /// </summary>
+    /// <param name="_eventData"></param>
+    public void OnPointerDown(PointerEventData _eventData)
+    {
+        OnDrag(_eventData);
+    }
+}
